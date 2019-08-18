6edb05a3e14b8f5d47d0c91442c88f1975f288b2
Thang Tran <baothangvn@gmail.com>
2019-08-18 15:05:29 -0500

    max character

---
+++ maximumcharacter.py
@@ -0,0 +1,20 @@
+def count_maximum(str):
+	ACSII = 256 
+
+	arr_count = [0] * ACSII
+
+	for character in str:
+		arr_count[ord(character)] = arr_count[ord(character)] + 1
+
+	max_value = -1
+	max_index = -1;
+	for i in range(len(arr_count)):
+		if arr_count[i] > max_value:
+			max_value = arr_count[i]
+			max_index = i
+	print(max(arr_count))
+	return max_index,max_value
+
+if __name__ == "__main__":
+	max_index,max_value = count_maximum("today is the first day in schoolddddddddddddddd")
+	print("{}: {}".format(chr(max_index),max_value))
