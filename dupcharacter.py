e9b5ee597dd15e061b5be0dadffcbd66d105d41e
Thang Tran <baothangvn@gmail.com>
2019-08-20 00:44:18 -0500

    dubplicatecharacter

---
+++ dupcharacter.py
@@ -0,0 +1,17 @@
+hash_table = {}
+
+
+def find_duplicate(str):
+	for i in range(len(str)):
+		if str[i] not in hash_table:
+			hash_table.update({str[i]:1})
+		else:
+			hash_table[str[i]] = hash_table[str[i]] + 1
+
+str = "Thang Tran khong biet chi het"
+find_duplicate(str);
+
+for key in hash_table:
+	if hash_table[key] >1:
+		print(key,hash_table[key])
+print(hash_table)
